407fad85f66835ab99430b0f210318f900bc0f37
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-07-24 17:00:50 +0600

    add form

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,76 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div>
+    <div>
+      <form @submit.prevent>
+        <p>Наименование товара</p>
+        <input
+          v-bind:value="title"
+          type="text"
+          placeholder="Введите наименование товара"
+          @input="title = $event.target.value"
+        />
+        <p>Описание товара</p>
+        <input
+          v-bind:value="body"
+          @input="body = $event.target.value"
+          type="text"
+          placeholder="Введите описание товара"
+        />
+        <p>Ссылка на изображение товара</p>
+        <input type="text" 
+        placeholder="Введите ссылку" />
+        <p>Цена товара</p>
+        <input v-bind:value="price"
+         type="number"
+          @input="price = $event.target.value"
+          placeholder="Введите цену" />
+        <button @click="createProduct">Добавить товар</button>
+      </form>
+    </div>
+    <div class="cart" v-for="cart in carts">
+      <div>{{ cart.title }}</div>
+      <div>{{ cart.body }}</div>
+      <div>{{ cart.price }}</div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  export default {
+    data(){
+      return{
+            carts: [
+        {
+          id: 1,
+          title: "Наименование товара",
+          body: "Довольно-таки интересное описание товара в несколько строк.Довольно-таки интересное описание товара в несколько строк. ",
+          price: 10000
+        }
+      ],
+       title: "",
+      body: "",
+      price: ""
+      }
+    },
+    methods:{
+      createProduct(event){
+        const newProduct = {
+          id: Date.now(),
+          title: this.title,
+          body: this.body,
+          price: this.price
+        };
+        this.carts.push(newProduct);
+        (this.title = ""), (this.body = ""), (this.price = "");
+      }
+    }
+  }
+</script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+<style >
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
